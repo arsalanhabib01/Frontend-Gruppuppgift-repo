baa267d6bb44a9f42828b8c34c55917301880fae
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-01 15:22:51 +0100

    template code to movie html files

--- afterWeCollided/afterWeCollided.js
+++ afterWeCollided/afterWeCollided.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
                   
-  $.getJSON('movies.json', function(json) {
+  $.getJSON('Frontend-Gruppuppgift-repo\movies.json', function(json) {
     $('.div1').append('<div class= "trailer">' + json.movies.afterWeCollide.trailer + '</div>');
     $('.div1').append('<p> Title: ' + json.movies.afterWeCollide.title + '</p>');
     $('.div1').append('<p> Production year : ' + json.movies.afterWeCollide.productionYear + '</p>');
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $('.div1').append('<p> Description: ' + json.movies.afterWeCollide.description + '</p>');
   });
 
-  $.getJSON('showings.json', function (json2) {
+  $.getJSON('Frontend-Gruppuppgift-repo\showings.json', function (json2) {
     let arr = json2.showings.afterWeCollided;
 
     $('.div2').append('<h2 class= "showings">Visningar:</h2>');
