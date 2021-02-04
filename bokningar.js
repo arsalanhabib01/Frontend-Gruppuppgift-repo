744149c3a006d80661c40b6abca2ac57c7ac3090
Arsalan Habib <71399565+arsalanhabib01@users.noreply.github.com>
2021-02-04 15:17:23 +0100

    bokning

--- bokningar.js
+++ bokningar.js
@@ -1,29 +1,89 @@
-let container = document.querySelector('.container');
-let count = document.getElementById('count');
-let price = document.getElementById('price');
 
-// Seat select event
-container.addEventListener('click', e => {
-  if (
-    e.target.classList.contains('seat') &&
-    !e.target.classList.contains('occupied')
-  ) {
-    e.target.classList.toggle('selected');
 
-    let selectedSeats = document.querySelectorAll('.row .selected');
-    let selectedSeatsCount = selectedSeats.length;
-    count.innerText = selectedSeatsCount;
+
+var container = document.querySelector('.container');
+var seats = document.querySelectorAll('.row .seat:not(.occupied)');
+var count = document.getElementById('count');
+var price = document.getElementById('price');
+var ageSelect = document.getElementById('age');
+var movieSelect = document.getElementById('movie');
+var tid = document.querySelector('.time');
+var selectedDay = document.getElementById('day');
+let selectedTime;
+var bookingArray = [];
+
+  // Seat select event
+  container.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('seat') &&
+      !e.target.classList.contains('occupied')
+    ) {
+      e.target.classList.toggle('selected');
+      updateSelectedSeatsCount();
+    }
+  });
+
+
+   tid.onclick = function () {
+     const rbs = document.querySelectorAll('input[name="contact"]');
+            for (const rb of rbs) {
+                if (rb.checked) {
+                    selectedTime = rb.value;
+                    break;
+                }
+            }
+
+        };
+
+
+
+
+function bookingSelectedSeats(){
+  var selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  var name = window.prompt("Enter your name to book your tickets: ");
+
+  if (name === null)
+    alert("Your booking is cancelled....");
+  else if (name === "")
+    alert("Must be entered the Name...");
+  else if (selectedTime === undefined)
+    alert("Please Select the time first...");
     
-    let movieSelect = document.getElementById('age');
-    let ticketPrice = +movieSelect.value;
-    price.innerText = selectedSeatsCount * ticketPrice;
-   // updateSelectedSeatsCount();
+  if (name !== null && name !== "" && selectedTime !== undefined
+    && selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('occupied');
+        var x = localStorage.removeItem('selectedSeats');
+      }
+    });
+    bookingArray.push({ "name": name }, { "seat no": selectedSeats }, { "time": selectedTime },
+      { "Total Price": price.innerText }, { "Number of tickets": count.innerText });
+    alert("Selected Seat no: " + selectedSeats + " booked by the name: " + name);
+    
+    // Mahran start from here jsonString i file .json
+    var jsonString = JSON.stringify(bookingArray);
+    // console.log(jsonString);
+     alert("JSON STRING : " +jsonString);
   }
-});
-
-// bokning button function
-function myButton() {
-  console.log("not working at the moment.....");
 }
 
-// working on it..............
+
+  function updateSelectedSeatsCount(){
+  var selectedSeats = document.querySelectorAll('.row .selected:not(.occupied)');
+  var seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  
+  var selectedSeatsCount = selectedSeats.length;
+  var ticketPrice = +ageSelect.value;
+  count.innerText = selectedSeatsCount;
+  price.innerText = selectedSeatsCount * ticketPrice;
+}
+
+
+  
+function myButton() {
+  bookingSelectedSeats();
+}
+
