c989959e1a87cf5be9ca074630131db5fb48ea07
AdrianCo97 <69801867+AdrianCo97@users.noreply.github.com>
2021-02-04 16:03:20 +0100

    Ändrade lite i bokningar.js

--- Bokningar/bokningar.js
+++ Bokningar/bokningar.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-  $.getJSON("bokningar.json", function (person) {
+  $.getJSON("bokningar.json", function (bokning) {
     for (let i = 0; i < data.length; i++) {
-      document.write('<div><p class="bokningar">' + person[i].namn + ' ' + person[i].sittplats + ' ' + person[i].datum + ' ' + person[i].tid + '</p></div>');
+      document.write('<div><p class="bokningar">' + bokning[i].namn + ' ' + bokning[i].sittplats + ' ' + bokning[i].datum + ' ' + bokning[i].tid + '</p></div>');
     };
   });
 });
