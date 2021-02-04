9a34e88d586a0aa600d2f166c2d2bff05856f1b5
AdrianCo97 <69801867+AdrianCo97@users.noreply.github.com>
2021-02-04 16:35:58 +0100

    Skapade en basic getjson till Biljetter.
    I denna så ska man kunna importera användarnas bokningshistorik från en json fil.

--- Bokningar/bokningar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $.getJSON("bokningar.json", function (bokning) {
-    for (let i = 0; i < data.length; i++) {
-      document.write('<div><p class="bokningar">' + bokning[i].namn + ' ' + bokning[i].sittplats + ' ' + bokning[i].datum + ' ' + bokning[i].tid + '</p></div>');
-    };
-  });
-});
