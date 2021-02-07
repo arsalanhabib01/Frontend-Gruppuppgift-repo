c47fd65b891abd210fdd514ddb9efbfdde27e8cf
AdrianCo97 <69801867+AdrianCo97@users.noreply.github.com>
2021-02-07 15:01:39 +0100

    Man får nu en bekräftelse på vilka val man gjort när man bokat en biljett för en film.

--- bokningar.js
+++ bokningar.js
@@ -14,33 +14,33 @@ let selectedDay = document.getElementById('day');
 let ticketDetails = [];
 let selectedTime;
 
-  // Seat select event
-  container.addEventListener('click', e => {
-    if (
-      e.target.classList.contains('seat') &&
-      !e.target.classList.contains('occupied')
-    ) {
-      e.target.classList.toggle('selected');
-      updateSelectedSeatsCount();
+// Seat select event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+    updateSelectedSeatsCount();
+  }
+});
+
+// time select event
+time.onclick = function () {
+  const rbs = document.querySelectorAll('input[name="contact"]');
+  for (const rb of rbs) {
+    if (rb.checked) {
+      selectedTime = rb.value;
+      break;
     }
-  });
+  }
 
-  // time select event
-   time.onclick = function () {
-     const rbs = document.querySelectorAll('input[name="contact"]');
-            for (const rb of rbs) {
-                if (rb.checked) {
-                    selectedTime = rb.value;
-                    break;
-                }
-            }
-
-        };
+};
 
 
 
 // this function book your tickets for the selected movie
-function bookingSelectedSeats(){
+function bookingSelectedSeats() {
   let selectedSeats = localStorage.getItem('selectedSeats');
   let name = window.prompt("Enter your name to book your tickets: ");
 
@@ -57,7 +57,7 @@ function bookingSelectedSeats(){
   else if (selectedTime === undefined)
     alert("Please fill all the required blanks...");
 
-    
+
   if (name !== null && name !== "" && selectedTime !== undefined && selectedMovie.value !== ""
     && selectedDay.value !== "" && selectedAge.value !== ""
     && selectedSeats !== null && selectedSeats.length > 0) {
@@ -70,22 +70,26 @@ function bookingSelectedSeats(){
     ticketDetails.push({ "Name": name }, { "Movie Name": selectedMovie.value }, { "Seat No": selectedSeats },
       { "Time": selectedTime }, { "Day": selectedDay.value }, { "Total Price": price.innerText },
       { "Number of Tickets": count.innerText });
-    alert("Ticket is booked by: " + name);
-    
+    alert("Ticket is now booked by: " + name + "\n" +
+      "Movie: " + selectedMovie.value + "\n" +
+      "Seat: " + selectedSeats + "\n" +
+      "Day: " + selectedDay.value + "\n" +
+      "Time: " + selectedTime);
+
 
     // Mahran kolla spara jsonString i file .json
-    let jsonString = JSON.stringify(ticketDetails,null,' ');
-     //console.log(jsonString);
+    let jsonString = JSON.stringify(ticketDetails, null, ' ');
+    //console.log(jsonString);
   }
 }
 
- // this function counts the number of seats and their total price 
-  function updateSelectedSeatsCount(){
+// this function counts the number of seats and their total price 
+function updateSelectedSeatsCount() {
   let selectedSeats = document.querySelectorAll('.row .selected:not(.occupied)');
   let seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-  
+
   let selectedSeatsCount = selectedSeats.length;
   let ticketPrice = +selectedAge.value;
   count.innerText = selectedSeatsCount;
