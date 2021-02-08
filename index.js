5f9d03ee096d6caf57bfdbe0c190681a04cabdf9
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-08 15:07:20 +0100

    Node install

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Create a new web server (based on express)
+
+const app = express();
+
+// Server all the files in the www folder (make new folder for all frontend material)
+app.use(express.static('www'));
+
+// Start the webserver on port 3000
+app.listen(3000, () => console.log('Listening on port 3000'));
