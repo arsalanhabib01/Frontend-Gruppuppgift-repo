5076a067863d0c12ddac1e2c4252d6c8621dd767
AdrianCo97 <69801867+AdrianCo97@users.noreply.github.com>
2021-02-11 12:06:26 +0100

    Importerade titlarna från json till selecten i bokningar.html

--- www/bokningar2.js
+++ www/bokningar2.js
@@ -1,1 +1,10 @@
-$.()
+$(document).ready(function () {
+
+  $.getJSON('moviesFilmersite.json', function (json) {
+    $.each(json, function (key, val) {
+      $('#movie').append('<option value=' + val.title + '>' + val.title + '</option>');
+    })
+  });
+
+
+})
