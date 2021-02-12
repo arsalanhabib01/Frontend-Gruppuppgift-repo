bf19fecf8e2bf850a4618819f622c0f96a81615d
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-12 11:53:48 +0100

    mediaquerys for movie pages for mobile devices

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // Server all the files in the www folder (make new folder for all frontend material)
-app.use(express.static('www'));
+app.use(express.static('Asset'));
 
 // Start the webserver on port 3000
 app.listen(3000, () => console.log('Listening on port 3000'));
